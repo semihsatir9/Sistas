f6a4c537edf24aa074f9e889fd3a2979469731ac
Semih Satır <semihsatir9@hotmail.com>
2023-10-02 18:11:27 +0300

    Initialization of authentication within the website.

---
+++ Sistas/Controllers/AuthController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Sistas.Models;
+
+namespace Sistas.Controllers
+{
+    public class AuthController : Controller
+    {
+
+        sistasEntities context = new sistasEntities();
+        // Authentication processes are done in this controller
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginModel info)
+        {
+            bool userExist = context.users.Any();
+            return View();
+        }
+
+
+        public ActionResult Signout()
+        {
+            return View();
+        }
+
+    }
+}
--- Sistas/Controllers/HomeController.cs
+++ Sistas/Controllers/HomeController.cs
@@ -8,23 +8,13 @@ namespace Sistas.Controllers
 {
     public class HomeController : Controller
     {
+
+        // Main page render
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
+        
     }
 }
