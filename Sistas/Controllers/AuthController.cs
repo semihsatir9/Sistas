5e0c25854124ece3e6381fe02ab80d948ad40e0b
Semih Satır <semihsatir9@hotmail.com>
2023-10-02 19:11:15 +0300

    Revert "Revert "Authentication added.""
    This reverts commit b198d1d43c7c95fd82da2230be962c199d86bc79.

--- Sistas/Controllers/AuthController.cs
+++ Sistas/Controllers/AuthController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Security;
+using System.Web.UI.WebControls;
 using Sistas.Models;
 
 namespace Sistas.Controllers
@@ -20,14 +22,32 @@ namespace Sistas.Controllers
         [HttpPost]
         public ActionResult Login(LoginModel info)
         {
-            bool userExist = context.users.Any();
+            // Checking to see if there is a user with these credentials.
+            bool userExist = context.users.Any(x => x.username == info.username && x.userpassword == info.password);
+
+            // The logged in user
+            user u = context.users.FirstOrDefault(x => x.username == info.username && x.userpassword == info.password);
+
+
+            if (userExist)
+            {
+                FormsAuthentication.SetAuthCookie(u.username, false);
+                return RedirectToAction("Index", "Home");
+            }
+
+
+            // Simple error message and show the same login page again
+            ModelState.AddModelError("", "Username or Password is wrong");
+            
+
             return View();
         }
 
 
         public ActionResult Signout()
         {
-            return View();
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
         }
 
     }
