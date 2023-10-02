576037b7eb3ea6c56188e9a83e84d3a2ffc16420
Semih Satır <semihsatir9@hotmail.com>
2023-10-02 18:38:28 +0300

    Authentication added.

--- Sistas/Controllers/HomeController.cs
+++ Sistas/Controllers/HomeController.cs
@@ -1,18 +1,27 @@
-﻿using System;
+﻿using Sistas.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
+
 
 namespace Sistas.Controllers
 {
     public class HomeController : Controller
     {
-
+        // Add the list and the alteration here
+        
         // Main page render
-        [Authorize]
+    [Authorize]
+
         public ActionResult Index()
         {
+            
+            sistasEntities context = new sistasEntities();
+            user table = context.users.FirstOrDefault(x => x.username == User.Identity.Name);
+
             return View();
         }
 
---
+++ Sistas/Models/UserDetails.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sistas.Models
+{
+    public class UserDetails
+    {
+        public int data1 { get; set; }
+        public int data2 { get; set; }
+        public int data3 { get; set; }
+        public int data4 { get; set; }
+    }
+}
